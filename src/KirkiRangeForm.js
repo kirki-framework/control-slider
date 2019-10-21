5bc5bb4fa25db243e978936bf91e12fd57018446
Ari Stathopoulos <aristath@gmail.com>
2019-10-21 21:31:17 +0300

    fixes https://github.com/wplemon/gridd/issues/114

--- src/KirkiRangeForm.js
+++ src/KirkiRangeForm.js
@@ -1,30 +1,90 @@
 /* globals _, wp, React */
 
 import InputRange from 'react-input-range';
+import reactCSS from 'reactcss';
+import NumberFormat from 'react-number-format';
 
 const KirkiRangeForm = ( props ) => {
 
-	const handleChangeComplete = ( val ) => {
-		wp.customize.control( props.customizerSetting.id ).setting.set( val );
+	const decimalPoints = () => {
+		if ( props.choices.step.toString().split( '.' )[1] ) {
+			return props.choices.step.toString().split( '.' )[1].length;
+		}
+		return 0;
 	};
 
-	const labelFormat = ( value ) => {
-		return props.choices.suffix ? value + props.choices.suffix : value;
+	const formatNumber = ( val ) => {
+		const pow = Math.pow( 10, decimalPoints() );
+		return Math.round( val * pow ) / pow;
+	}
+
+	const formatValue = ( val ) => {
+		return Math.min( props.choices.max, Math.max( props.choices.min, formatNumber( val ) ) );
 	};
+
+	props.choices.min = formatNumber( props.choices.min );
+	props.choices.max = formatNumber( props.choices.max );
+	props.choices.step = formatNumber( props.choices.step );
+
+	props.value = formatValue( props.value );
+
+	props.choices.prefix = props.choices.prefix || '';
+	props.choices.suffix = props.choices.suffix || '';
+
+	const handleChangeComplete = ( val ) => {
+		wp.customize.control( props.customizerSetting.id ).setting.set( formatValue( val ) );
+	};
+
+	const handleChangeCompleteTextInput = ( values ) => {
+		const {formattedValue, value} = values;
+		wp.customize.control( props.customizerSetting.id ).setting.set( formatValue( value ) );
+	};
+
+	// Styles.
+	const styles = reactCSS( {
+		default: {
+			labelsWrapper: {
+				display: 'flex',
+				'justify-content': 'space-between',
+				color: '#aaaaaa',
+				'font-size': '10px',
+			},
+
+			valueLabel: {
+				color: '#555d66',
+				'font-weight': '700',
+				'font-size': '13px'
+			}
+		}
+	} );
 
 	return (
 		<div>
 			<label className="customize-control-title">{ props.label }</label>
 			<span class="description customize-control-description" dangerouslySetInnerHTML={{ __html: props.description }}></span>
 			<div className="customize-control-notifications-container" ref={ props.setNotificationContainer }></div>
-			<InputRange
-				maxValue={ props.choices.max }
-				minValue={ props.choices.min }
-				step={ props.choices.step }
-				value={ props.value }
-				onChange={ handleChangeComplete }
-				formatLabel={ labelFormat }
-			/>
+			<div class="range-input-wrapper">
+				<div style={ styles.labelsWrapper } className={ 'manual-input-field' }>
+					<span>{ props.choices.prefix }{ props.choices.min }{ props.choices.suffix }</span>
+					<span style={ styles.valueLabel }>
+						<NumberFormat
+							value={ props.value }
+							displayType={ 'input' }
+							prefix={ props.choices.prefix }
+							suffix={ props.choices.suffix }
+							onValueChange={ handleChangeCompleteTextInput }
+						/>
+					</span>
+					<span>{ props.choices.prefix }{ props.choices.max }{ props.choices.suffix }</span>
+				</div>
+				<InputRange
+					maxValue={ props.choices.max }
+					minValue={ props.choices.min }
+					step={ props.choices.step }
+					value={ props.value }
+					onChange={ handleChangeComplete }
+				/>
+			</div>
 		</div>
 	);
 };
